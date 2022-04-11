39f05e5aea2cdfb28d7fa0b0ac3781c949296c3e
LidaRP <rahmanipiyani.lida@gmail.com>
2022-04-11 13:40:00 +0430

    Salary tax

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+         {
+         	int x,y;
+         	cout<<"please enter your salary:"<<endl;
+         	cin>>x;
+         	cout<<"What is the tax rate?"<<endl;
+         	cin>>y;
+         	cout<<(x*y)/100;
+	return 0;
+         }
